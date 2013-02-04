ae427a2f825283c8ed50f6a04fc9b9b7c8e84942
Pierre-Alexis de Solminihac <pa@solminihac.fr>
2013-02-04 01:48:18 +0100

    restoring contact-2.2 branch

--- view/contact/index.php
+++ view/contact/index.php
@@ -2,10 +2,10 @@
 if (!isset($data['class'])) {
     $data['class'] = 'contact';
 }
-$this->getModel('cssjs')->register_foot('clementine_' . $data['class'] . '-submit', $this->getBlockHtml($data['class'] . '/js_submit', $data));
+$this->getModel('cssjs')->register_foot('clementine_contact-submit', $this->getBlockHtml($data['class'] . '/js_submit', $data));
 $conf = '';
-if (isset($data['config_module_' . $data['class']])) {
-    $conf = $data['config_module_' . $data['class']];
+if (isset($data['config_module_contact'])) {
+    $conf = $data['config_module_contact'];
 }
 // par défaut
 if (!$conf) {
@@ -14,27 +14,29 @@ if (!$conf) {
 $ns = $this->getModel('fonctions');
 if ($ns->ifSet('message', 'get')) {
 ?>
+<div class="error"><strong>
 <?php
     $err = $ns->ifGet('int', 'message');
     $message = '';
     switch ($err) {
         case 1 :
-            $message = '<h2 class="error">Erreur</h2><p class="error">Le message n\'a pas été envoyé</p>';
+            $message = "Le message n'a pas été envoyé";
             break;
         case 2 :
             $message = "Le message a bien été envoyé";
             break;
         default :
-            $message = '<h2>Erreur</h2><p class="error">Le message n\'a pas pu être envoyé</p>';
+            $message = "Le message n'a pu être envoyé";
             break;
     }
     echo $message;
 ?>
-    <br /><br />
+    <br /></strong><br />
+</div>
 <?php
 } else {
 ?>
-    <form id="form_<?php echo $data['class']; ?>" name="form_<?php echo $data['class']; ?>" class="form_contact form_<?php echo $data['class']; ?>" action="<?php echo __WWW__; ?>/<?php echo $data['class']; ?>/post" method="post" accept-charset="utf-8">
+    <form id="form_contact" name="form_contact" class="form_contact" action="<?php echo __WWW__; ?>/<?php echo $data['class']; ?>/post" method="post" accept-charset="utf-8">
 <?php
     $this->getBlock($data['class'] . '/index_fields', $data);
 ?>
